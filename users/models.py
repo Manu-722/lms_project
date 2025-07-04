8c61856494e4bece517ee06bb7ecc36d102b5bac
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-04 22:29:49 +0300

    added the views,serializers and added the installed apps

--- users/models.py
+++ users/models.py
@@ -1,4 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import Abstract User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('instructor', 'Instructor'),
+        ('student', 'Student'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserRegistrationSerializer
+
 
 # Create your views here.
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegistrationSerializer
