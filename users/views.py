0738fac187553926ecde61cf708225bcb44cec6d
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-04 23:07:30 +0300

    defined a function for users

--- users/views.py
+++ users/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserRegistrationSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 
 # Create your views here.
 class UserRegistrationView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserRegistrationSerializer
+class ProtectedUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "message": f"Hello, {request.user.username}!",
+            "role": request.user.role
+        })
