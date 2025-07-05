f237fbee961324033c59f189d98622c3fe9b1934
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-05 21:56:57 +0300

    did migrations

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, ProtectedUserView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
+    path('me/', ProtectedUserView.as_view(), name='user-profile'),
 ]
+
