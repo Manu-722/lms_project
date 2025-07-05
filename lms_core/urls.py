f26826c983c2018d1b0fbf5bb9a19f2d833c8df5
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-05 23:23:42 +0300

    added paths

--- lms_core/urls.py
+++ lms_core/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/courses/', include('courses.urls')),
+
 
 
 ]
