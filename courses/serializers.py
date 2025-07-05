3463515ab5b15e934bcd5c383d71aafd40a40201
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-05 23:20:29 +0300

    craeted a serializer for courses and enrollment

--- courses/serializers.py
+++ courses/serializers.py
@@ -11,6 +11,5 @@ class CourseSerializer(serializers.ModelSerializer):
 class EnrollmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Enrollment
-        fields = '__all__'
-
- 
+        fields = ['id', 'student', 'course', 'enrolled_at']
+        read_only_fields = ['student', 'enrolled_at']
