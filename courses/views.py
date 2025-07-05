9d80ed834849d5be2225b1588d81d83ced643c58
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-05 23:22:36 +0300

    created views for instructor,student and classlist

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Course, Enrollment
+from .serializers import CourseSerializer, EnrollmentSerializer
+from users.models import CustomUser
+
 
 # Create your views here.
+class IsInstructor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'instructor'
+
+class IsStudent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'student'
+class CourseListCreateView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsInstructor()]
+        return [permissions.AllowAny()]
+
+    def perform_create(self, serializer):
+        serializer.save(instructor=self.request.user)
+
+class EnrollmentCreateView(generics.CreateAPIView):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+    permission_classes = [IsStudent]
+
+    def perform_create(self, serializer):
+        serializer.save(student=self.request.user)
