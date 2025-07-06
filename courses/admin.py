228b1db45dbe7f04fe60127ea2a176c730af4061
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-07 00:13:15 +0300

    registered models

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Lecture, Assignment, Submission
 
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(Enrollment)
+admin.site.register(Lecture)
+admin.site.register(Assignment)
+admin.site.register(Submission)
