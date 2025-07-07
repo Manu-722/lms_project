abfa0b18439291aaca16028bbace19b8dd5f3b31
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-07 18:47:03 +0300

    serializer for the submission

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Course, Enrollment, Lecture, Assignment
+from .models import Course, Enrollment, Lecture, Assignment, Submission
 from users.models import CustomUser
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -28,10 +28,10 @@ class AssignmentSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['course', 'created_at']
 
-from .models import Submission
+
 
 class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
-        fields = ['id', 'assignment', 'student', 'content', 'submitted_at']
+        fields = ['id', 'assignment', 'student', 'content', 'file', 'submitted_at', 'grade', 'feedback']
         read_only_fields = ['student', 'submitted_at']
