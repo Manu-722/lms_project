7539a2f30652422c3840d58771a2d084dada1afc
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-07 18:45:33 +0300

    added the submission model

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import CustomUser
+from django.core.validators import FileExtensionValidator
 
 
 # Create your models here.
@@ -40,15 +41,17 @@ class Assignment(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.course.title})"
-    
+
+
 class Submission(models.Model):
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name='submissions')
     student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, limit_choices_to={'role': 'student'})
     content = models.TextField()
+    file = models.FileField(upload_to='submissions/', null=True, blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'docx', 'png', 'jpg'])])
     submitted_at = models.DateTimeField(auto_now_add=True)
+    grade = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    feedback = models.TextField(blank=True)
 
     class Meta:
         unique_together = ('assignment', 'student')
-
-    def __str__(self):
-        return f"{self.student.username} → {self.assignment.title}"
