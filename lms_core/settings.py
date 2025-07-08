1d809d8240d656e33a38c1c3be9124e52cb5a2fe
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-07-08 22:52:46 +0300

    added allowed hosts

--- lms_core/settings.py
+++ lms_core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#v#+=1v8q$+m24x6t_kt()!n=l=fh+!3npcpu#h=j-dxz)kkiv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
