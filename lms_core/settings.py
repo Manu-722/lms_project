9910608b5cb8ff1db6843a42fc3cc0bed0e804e3
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-08-03 15:43:16 +0300

    removed secrets

--- lms_core/settings.py
+++ lms_core/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#v#+=1v8q$+m24x6t_kt()!n=l=fh+!3npcpu#h=j-dxz)kkiv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
